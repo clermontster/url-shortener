e35e709cf16dc3b3a969518ff3374b0b0ce72385
Jonnie Clermont <clermontster@gmail.com>
2017-10-25 22:54:27 -0500

    update mongodb url

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 // const url = 'mongodb://localhost:27017/url-shortener';	
-const url = 'mongodb://<dbuser>:<dbpassword>@ds235775.mlab.com:35775/url-shortener';
+// const url = 'mongodb://<dbuser>:<dbpassword>@ds235775.mlab.com:35775/url-shortener';
+const url = process.env.MONGOLAB_URI;
 
 const indexRoute = require('./routes/indexRoute');
 const newRoute = require('./routes/newRoute');
